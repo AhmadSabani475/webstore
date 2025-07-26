e9ae96bc58a9d27ea0b3faec4ebaeb089dcf7262
AhmadSabani475 <sabanirizki275@gmail.com>
2025-07-27 00:16:30 +0700

    product-catalog

---
+++ app/Livewire/ProductCatalog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Data\ProductData;
+use App\Models\Product;
+use Livewire\Component;
+
+class ProductCatalog extends Component
+{
+    public function render()
+    {
+        $result = Product::paginate(1); // orm / Database Query
+        $products = ProductData::collect($result);
+
+        return view('livewire.product-catalog', compact('products'));
+    }
+}
